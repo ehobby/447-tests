ce2c0a5e7a7b54065d7e8d82a8577172e2b47351
ehobby <hobbyemily@gmail.com>
2019-05-07 09:51:56 -0400

    dim

--- js/cell.js
+++ js/cell.js
@@ -114,14 +114,14 @@ function cellUniverse() {
         var width = canvasGrid.offsetWidth;
         console.log(height);
         console.log(width);
-        for (var x = 0; x <= width; x += CELL_DIMENSION * 4) {
-            context.moveTo(x + PADDING, PADDING);
-            context.lineTo(x + PADDING, height - PADDING);
+        for (var x = 0; x <= width; x += CELL_DIMENSION) {
+            context.moveTo((x + PADDING), PADDING);
+            context.lineTo(x, (height - PADDING));
         }
 
-        for (var x = 0; x <= height; x += CELL_DIMENSION * 4) {
-            context.moveTo(PADDING, x + PADDING);
-            context.lineTo(width  + PADDING, x - PADDING);
+        for (var x = 0; x <= height; x += CELL_DIMENSION) {
+            context.moveTo(PADDING, (x + PADDING));
+            context.lineTo((width + PADDING), x);
         }
         context.strokeStyle = "black";
         context.stroke();
