42fd24768412b1c36d3ff05e33c5c06c3575a645
ehobby <hobbyemily@gmail.com>
2019-05-07 10:46:14 -0400

    coloring

--- js/cell.js
+++ js/cell.js
@@ -437,7 +437,9 @@ function generateSelectedCells() {
 
 function updateCellAppearance(y, x, status) {
     var canvasGrid = document.getElementById(CELL_GRID_ID);
-    canvasGrid.rows[y].cells[x].style.backgroundColor = COLORS[status];
+    const context = canvas.getContext('2d');
+    context.fillStyle = COLORS[status];
+    context.fillRect(y, x, CELL_DIMENSION / 2, CELL_DIMENSION / 2);
 }
 
 function parseCellID(id) {
