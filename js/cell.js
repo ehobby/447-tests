0bd95dc39183ac163ef6866d5393b91195811a88
ehobby <hobbyemily@gmail.com>
2019-05-07 09:06:46 -0400

    first try

--- js/cell.js
+++ js/cell.js
@@ -82,7 +82,7 @@ function cellUniverse() {
             }
         }
     }
-
+/*
     this.generateTable = function () {
         var tableGrid = document.getElementById(CELL_GRID_ID); //GENERATE TABLE SHOULD DECIDE ROWS/COLS
         tableGrid.innerHTML = "";
@@ -104,7 +104,28 @@ function cellUniverse() {
         }
 
        
+    }*/
+   
+    this.generateGrid = function () {
+        var canvasGrid = document.getElementById(CELL_GRID_ID); //GENERATE TABLE SHOULD DECIDE ROWS/COLS
+        canvasGrid.innerHTML = "";
+        var context = canvasGrid.getContext("2d");
+        var numRows = this.bottomBound;
+        var numCols = this.rightBound;
+        for (var x = 0; x <= bw; x += 40) {
+            context.moveTo(0.5 + x + p, p);
+            context.lineTo(0.5 + x + p, bh + p);
+        }
+
+        for (var x = 0; x <= bh; x += 40) {
+            context.moveTo(p, 0.5 + x + p);
+            context.lineTo(bw + p, 0.5 + x + p);
+        }
+        context.strokeStyle = "black";
+        context.stroke();
     }
+
+
 
     this.resize = function (newDim) {
         if (newDim > MAX_DIM) {
@@ -117,7 +138,7 @@ function cellUniverse() {
         }
         CELL_DIMENSION = newDim;
         this.updateBounds();
-        this.generateTable();
+        this.generateGrid();
         this.generateCells();
         generateSelectedCells();
     }
